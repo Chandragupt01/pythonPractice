a1c7157134818a13a69bc530609489e48d4eb3c6
Chandragupt01 <chandraguptacharya02@gmail.com>
2024-01-20 04:24:30 +0530

    substringCount

---
+++ substringCount.py
@@ -0,0 +1,7 @@
+# Return the count of a given substring from a string
+'''Write a program to find how many times substring “Emma” appears in the given string.'''
+
+str_x=input("Enter a string: ")
+substring=input("Enter a substring: ")
+substringCount=str_x.count(substring)
+print(substringCount)
