27871e28a831c3b8e5d53f5102287d4dfdd40574
Chandragupt01 <chandraguptacharya02@gmail.com>
2024-01-23 23:16:09 +0530

    oneInputString

---
+++ oneInputString.py
@@ -0,0 +1,7 @@
+# Accept any three string from one input() call
+'''Write a program to take three names as input from a user in the single input() function call.'''
+
+str1,str2,str3=input("Enter three strings seperated by '':").split()
+print(str1)
+print(str2)
+print(str3)
