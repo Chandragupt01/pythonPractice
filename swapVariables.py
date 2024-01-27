b62cb17fce069cdc7733a50f949942eff03962a7
Chandragupt01 <chandraguptacharya02@gmail.com>
2024-01-27 20:32:06 +0530

    swapVariables

---
+++ swapVariables.py
@@ -0,0 +1,12 @@
+# Python Program to Swap Two Variables
+'''Python Program to Swap Two Variables'''
+
+x = int(input("Enter a number: "))
+y = int(input("Enter a number: "))
+
+print("x: ",x,"y: ", y)
+temp=x
+x=y
+y=temp
+
+print("x: ",x,"y: ", y)
