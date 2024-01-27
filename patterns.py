b8af12ec35a1e62016531ca9445eb0c566005924
Chandragupt01 <chandraguptacharya02@gmail.com>
2024-01-27 23:38:48 +0530

    patterns

--- patterns.py
+++ patterns.py
@@ -7,8 +7,50 @@
 5 5 5 5 5
 '''
 
-n=int(input("Enter a number: "))
-for i in range(n+1):
-    for j in range(i):
-        print(i ,end='')
-    print('')
+# n=int(input("Enter a number: "))
+# for i in range(n+1):
+#     for j in range(i):
+#         print(i ,end='')
+#     print('')
+
+'''
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5
+'''
+
+# n=int(input("Enter a number: "))
+# for i in range(n+1):
+#     for j in range(1,i+1):
+#         print(j ,end='')
+#     print('')
+
+'''
+1 1 1 1 1 
+2 2 2 2 
+3 3 3 
+4 4 
+5
+'''
+
+# n=int(input("Enter a number: "))
+# for i in range(1,n+1):
+#     for j in range(n-i+1):
+#         print(i ,end='')
+#     print('')
+
+'''
+0 1 2 3 4 5 
+0 1 2 3 4 
+0 1 2 3 
+0 1 2 
+0 1
+'''
+
+# n=int(input("Enter a number: "))
+# for i in range(0,n):
+#     for j in range(n-i+1):
+#         print(j ,end='')
+#     print('')
