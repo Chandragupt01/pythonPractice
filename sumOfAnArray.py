c3bb13d407656a166f13ed10daedf3c9d5af938e
Chandragupt01 <chandraguptacharya02@gmail.com>
2024-01-28 22:23:18 +0530

    sumOfAnArray

---
+++ sumOfAnArray.py
@@ -0,0 +1,14 @@
+# Python Program to Find Sum of Array
+'''Given an array of integers, find the sum of its elements.
+
+Examples:
+
+Input : arr[] = {1, 2, 3}
+Output : 6
+Explanation: 1 + 2 + 3 = 6'''
+
+arr=[2,4,8,34,88,9]
+sum=0
+for item in arr:
+    sum=sum+item
+print("Sum is: ", sum)
